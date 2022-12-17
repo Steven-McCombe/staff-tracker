0264821e402435f2f696e0490f165c5d8a8f1875
Steven-McCombe <smccombe93@gmail.com>
2022-12-17 14:30:16 -0500

    added server boiler plate and Schema.sql to create the database

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+
+-- drop and create database
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+-- Use this database
+USE employee_db;
+
+-- create the department table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
+-- create the role table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- Create the employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
